778cf18268465aa9baf4d176415c2f51dacc8e47
Victor Navarro <vnavarrosm@gmail.com>
2016-04-27 16:53:40 -0400

    Crea modelo de producto y crea migracion para la base de datos

---
+++ database/migrations/2016_04_20_205239_producto.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Producto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->integer('precio');
+            $table->string('tipo');
+            $table->string('unidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('productos');
+    }
+}
