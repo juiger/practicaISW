ae81974b5f9f0cdbf17ffc00c54e18a50f72bb8c
Victor Navarro <vnavarrosm@gmail.com>
2016-06-06 23:48:57 -0400

    agrega campo de selección para tipo de producto

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -8,12 +8,12 @@
     {!! Form::text('precio') !!}
 </div>
 <div>
-    {!! Form::label('tipo', 'Tipo') !!}
-    {!! Form::text('tipo') !!}
-</div>
-<div>
     {!! Form::label('unidad', 'Unidad') !!}
     {!! Form::text('unidad') !!}
+</div>
+<div>
+    {!! Form::label('tipo_producto_id','Tipo de Producto') !!}
+    {!! Form::select('tipo_producto_id',App\TipoProducto::pluck('nombre','id')) !!}
 </div>
 <div>
     {!! Form::submit('Guardar') !!}
