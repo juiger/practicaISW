2cc3583df344958b71415f716b656d95e465dd0e
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 21:40:41 -0400

    agrega Getter para Tipo

--- app/Producto.php
+++ app/Producto.php
@@ -18,5 +18,9 @@ class Producto extends Model
     {
         return $this->belongsTo('App\TipoProducto');
     }
+
+    public function getTipoAttribute(){
+        return !is_null($this->tipo_producto)?$this->tipo_producto->nombre:'ERROR';
+    }
     
 }
