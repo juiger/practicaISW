2ed122bc139a5940ff0584944c7875a2485f12ac
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 23:22:32 -0400

    limpia espacios en blanco

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,9 +16,6 @@ Route::get('/', function () {
 });
 
 Route::get('boletas/{boleta}/agregar_producto','BoletasController@vista_agregar_producto')->name('boletas.vista_agregar_producto');
-
 Route::post('boletas/{boleta}/agregar_producto','BoletasController@agregar_producto')->name('boletas.agregar_producto');
-
-
 Route::resource('productos','ProductosController');
 Route::resource('boletas','BoletasController');
