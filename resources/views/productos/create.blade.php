0efab235733dcaaed11c67bf34ba64d91de22755
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 22:52:49 -0400

    agrega herencia de master y estilos a vista productos.create

--- resources/views/productos/create.blade.php
+++ resources/views/productos/create.blade.php
@@ -1,21 +1,38 @@
-{!! Form::open(array('route' => 'productos.store')) !!}
-<div>
-    {!! Form::label('nombre', 'Nombre') !!}
-    {!! Form::text('nombre') !!}
-</div>
-<div>
-    {!! Form::label('precio', 'Precio') !!}
-    {!! Form::text('precio') !!}
-</div>
-<div>
-    {!! Form::label('unidad', 'Unidad') !!}
-    {!! Form::text('unidad') !!}
-</div>
-<div>
-    {!! Form::label('tipo_producto_id','Tipo de Producto') !!}
-    {!! Form::select('tipo_producto_id',App\TipoProducto::pluck('nombre','id')) !!}
-</div>
-<div>
-    {!! Form::submit('Guardar') !!}
-</div>
-{!! Form::close() !!}
+@extends('master')
+@section('titulo')
+    Crear Producto
+@endsection
+@section('contenido')
+    {!! Form::open(array('route' => 'productos.store','class'=>'form-horizontal')) !!}
+    <div class="form-group row">
+        {!! Form::label('nombre', 'Nombre', ['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+        {!! Form::text('nombre','',['class'=>'form-control']) !!}
+        </div>
+    </div>
+    <div class="form-group row">
+        {!! Form::label('precio', 'Precio',['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+        {!! Form::text('precio','',['class'=>'form-control']) !!}
+        </div>
+    </div>
+    <div class="form-group">
+        {!! Form::label('unidad', 'Unidad',['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+        {!! Form::text('unidad','',['class'=>'form-control']) !!}
+        </div>
+
+    </div>
+    <div class="form-group">
+        {!! Form::label('tipo_producto_id','Tipo de Producto',['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+        {!! Form::select('tipo_producto_id',App\TipoProducto::pluck('nombre','id'),null,['class'=>'form-control']) !!}
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-3 col-sm-offset-3">
+        {!! Form::submit('Guardar',['class'=>'form-control btn btn-primary']) !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
+@endsection
