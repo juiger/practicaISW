8bb3388571739dc1d31870abdd12db0bd19913b3
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 22:09:02 -0400

    Agrega estilos a la vista productos.index y botones de acción usando javascript

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,16 +1,46 @@
-<table>
-    <tr>
-        <th>Nombre</th>
-        <th>Precio</th>
-        <th>Tipo</th>
-        <th>Unidad</th>
-    </tr>
+@extends('master')
+@section('titulo')
+    Productos
+@endsection
+@section('contenido')
+<table class="table table-bordered table-striped table-hover">
+    <thead>
+        <tr>
+            <th class="col-md-6">Nombre</th>
+            <th class="col-md-5">Tipo</th>
+            <th class="col-md-1">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
     @foreach($productos as $producto)
-    <tr>
-        <td>{{ $producto->nombre }}</td>
-        <td>{{ $producto->precio }}</td>
-        <td>{{ $producto->tipo_producto->nombre or 'ERROR' }}</td>
-        <td>{{ $producto->unidad }}</td>
-    </tr>
+        <tr>
+            <td>{{ $producto->nombre }}</td>
+            <td>{{ $producto->tipo }}</td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            <button class="btn-xs btn-success" onclick="location.href='{{route('productos.show',[$producto->id])}}'">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </td>
+                        <td>
+                            <button class="btn-xs btn-primary" onclick="location.href='{{route('productos.edit',[$producto->id])}}'">
+                                <span class="glyphicon glyphicon-pencil"></span>
+                            </button>
+                        </td>
+                        <td>
+                            {!! Form::model($producto, array('route' => array('productos.destroy', $producto->id),'method'=>'DELETE')) !!}
+                            <button type="submit" class="btn-xs btn-danger" onClick="return confirm('Está Seguro?')">
+                                <span class="glyphicon glyphicon-trash" aria-label="Eliminar"></span>
+                            </button>
+                            {!! Form::close() !!}
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    <tbody>
     @endforeach
 </table>
+@endsection
