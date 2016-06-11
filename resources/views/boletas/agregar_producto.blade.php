c900e090abbcb6ec5bb5db7b2b78989fdfddd173
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 23:20:52 -0400

    agrega estilos a vista boletas.agregar_producto

--- resources/views/boletas/agregar_producto.blade.php
+++ resources/views/boletas/agregar_producto.blade.php
@@ -1,11 +1,25 @@
-{!! Form::open(array('route' => ['boletas.agregar_producto',$boleta->id])) !!}
-<div>
-    {!! Form::label('producto_id', 'Nombre') !!}
-    {!! Form::select('producto_id', App\Producto::lists('nombre','id')) !!}
-</div>
-<div>
-    {!! Form::label('cantidad', 'Cantidad') !!}
-    {!! Form::number('cantidad') !!}
-</div>
-{!! Form::submit('Enviar') !!}
-{!! Form::close() !!}
+@extends('master')
+@section('titulo')
+    Crear Boleta
+@endsection
+@section('contenido')
+    {!! Form::open(array('route' => ['boletas.agregar_producto',$boleta->id], 'class'=>'form-horizontal')) !!}
+    <div class="form-group row">
+        {!! Form::label('producto_id', 'Nombre',['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+            {!! Form::select('producto_id', App\Producto::lists('nombre','id'),null,['class'=>'form-control']) !!}
+        </div>
+    </div>
+    <div class="form-group row">
+        {!! Form::label('cantidad', 'Cantidad',['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+            {!! Form::number('cantidad',null,['class'=>'form-control']) !!}
+        </div>
+    </div>
+    <div class="form-group row">
+        <div class="row col-md-offset-3 col-md-3">
+            {!! Form::submit('Enviar',['class'=>'btn btn-primary']) !!}
+        </div>
+    </div>
+    {!! Form::close() !!}
+@endsection
