78579fd239259672ece938fe11d3e41f0c7847b2
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 23:59:21 -0400

    agrega nombre a ruta home

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('boletas/{boleta}/agregar_producto','BoletasController@vista_agregar_producto')->name('boletas.vista_agregar_producto');
 Route::post('boletas/{boleta}/agregar_producto','BoletasController@agregar_producto')->name('boletas.agregar_producto');
