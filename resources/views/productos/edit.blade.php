6cdeed11b4c02d1cdd676f723948728de5e73a23
Victor Navarro <vnavarrosm@gmail.com>
2016-06-10 23:18:09 -0400

    agrega estilos a vista productos.edit

--- resources/views/productos/edit.blade.php
+++ resources/views/productos/edit.blade.php
@@ -1,21 +1,38 @@
-{!! Form::model($producto, array('route' => array('productos.update', $producto->id),'method'=>'PUT')) !!}
-<div>
-    {!! Form::label('nombre', 'Nombre') !!}
-    {!! Form::text('nombre') !!}
+@extends('master')
+@section('titulo')
+    Editar Producto: {{$producto->nombre}}
+@endsection
+@section('contenido')
+{!! Form::model($producto, array('route' => array('productos.update', $producto->id),'method'=>'PUT','class'=>'form-horizontal')) !!}
+<div class="form-group row">
+    {!! Form::label('nombre', 'Nombre', ['class'=>'control-label col-md-3']) !!}
+    <div class="col-md-9">
+        {!! Form::text('nombre',null,['class'=>'form-control']) !!}
+    </div>
 </div>
-<div>
-    {!! Form::label('precio', 'Precio') !!}
-    {!! Form::text('precio') !!}
+<div class="form-group row">
+    {!! Form::label('precio', 'Precio',['class'=>'control-label col-md-3']) !!}
+    <div class="col-md-9">
+        {!! Form::text('precio',null,['class'=>'form-control']) !!}
+    </div>
 </div>
-<div>
-    {!! Form::label('tipo', 'Tipo') !!}
-    {!! Form::text('tipo') !!}
+<div class="form-group">
+    {!! Form::label('unidad', 'Unidad',['class'=>'control-label col-md-3']) !!}
+    <div class="col-md-9">
+        {!! Form::text('unidad',null,['class'=>'form-control']) !!}
+    </div>
+
 </div>
-<div>
-    {!! Form::label('unidad', 'Unidad') !!}
-    {!! Form::text('unidad') !!}
+<div class="form-group">
+    {!! Form::label('tipo_producto_id','Tipo de Producto',['class'=>'control-label col-md-3']) !!}
+    <div class="col-md-9">
+        {!! Form::select('tipo_producto_id',App\TipoProducto::pluck('nombre','id'),null,['class'=>'form-control']) !!}
+    </div>
 </div>
-<div>
-    {!! Form::submit('Guardar') !!}
+<div class="form-group">
+    <div class="col-md-3 col-sm-offset-3">
+        {!! Form::submit('Guardar',['class'=>'form-control btn btn-primary']) !!}
+    </div>
 </div>
 {!! Form::close() !!}
+@endsection
