0508dc407f54744da47c1531185d80f32ce4072e
Victor Navarro <vnavarrosm@gmail.com>
2016-06-15 16:46:53 -0400

    agrega campo rut a boleta

--- resources/views/boletas/create.blade.php
+++ resources/views/boletas/create.blade.php
@@ -10,6 +10,12 @@
             {!! Form::text('cajero',null,['class'=>'form-control']) !!}
         </div>
     </div>
+    <div class="form-group">
+        {!! Form::label('rut', 'Rut',['class'=>'control-label col-md-3']) !!}
+        <div class="col-md-9">
+            {!! Form::text('rut',null,['class'=>'form-control']) !!}
+        </div>
+    </div>
     <div class="form-group row">
         <div class="col-md-offset-3 col-md-3 ">
             {!! Form::submit('Guardar',['class'=>'form-control btn btn-primary']) !!}
