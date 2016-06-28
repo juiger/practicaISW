10020c5c7eeedc7b85cf9fad287f2a5b81271c22
Victor Navarro <vnavarrosm@gmail.com>
2016-06-28 08:32:45 -0400

    agrega verificación de autenticación a los controladores de Boletas y Productos

--- app/Http/Controllers/BoletasController.php
+++ app/Http/Controllers/BoletasController.php
@@ -10,6 +10,12 @@ use App\Http\Requests;
 
 class BoletasController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
